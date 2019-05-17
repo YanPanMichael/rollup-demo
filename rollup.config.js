1d4c5c1d8bf63fe8f3005fec720eabd13c8599cb
YanPanMichael <yanpancsu@icloud.com>
2019-05-17 09:20:24 +0800

    add index html page

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,13 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
-  entry: 'src/index.js',
-  format: 'umd',
-  plugins: [
-    resolve(),
-    babel({
-      excludes: "node_modules/**"
-    })
-  ],
-  dest: 'build/bundle.js'
+    entry: 'src/index.js',
+    format: 'umd',
+    plugins: [
+        resolve(),
+        babel({
+            exclude: 'node_modules/**'
+        })
+    ],
+    dest: 'build/bundle.js'
 }
